b9b68a395d12f4990272d0ee70facf3cdb907b3e
BrithanMP <brithanmurillo15@gmail.com>
2026-09-04 08:41:26 -0600

    Agregar nota UI

--- app/src/main/java/com/my/first/appdenotas/AgregarNotaActivity.kt
+++ app/src/main/java/com/my/first/appdenotas/AgregarNotaActivity.kt
@@ -1,32 +1,20 @@
 package com.my.first.appdenotas
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.my.first.appdenotas.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
+class AgregarNotaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+        setContentView(R.layout.activity_agregar_nota)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        binding.FABAgregarNota.setOnClickListener {
-         startActivity(Intent(applicationContext, AgregarNotaActivity::class.java))
         }
     }
 }
